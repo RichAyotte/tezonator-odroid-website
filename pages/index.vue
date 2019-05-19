595368209cb0e3927124553a319c2ba03a1a6552
Richard Ayotte <rich@ayottesoftware.com>
2019-05-19 06:38:40 -0400

    Add content to home page  - Add tezonator photo, logo and sd img download link

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,51 @@
 <template>
-	<section class="container">
-		<div>
-			<logo />
-			<h1 class="title">
-				The Tezonator
-			</h1>
-			<h2 class="subtitle">
-				ODroid N2 Tezos Baker
-			</h2>
-		</div>
-	</section>
+	<div>
+		<header>
+			<img
+				src="~assets/tezonator-logo.png"
+				class="logo"
+			>
+		</header>
+		<section>
+			<img
+				class="tezonator-photo"
+				src="~assets/tezonator-odroid-n2.jpg"
+			>
+			<a
+				class="button"
+				href="/odroid-images/tezonator-light-final.img.xz"
+			>Download ODroid-N2 microSD image</a>
+		</section>
+		<footer>
+
+		</footer>
+	</div>
 </template>
+
+<style lang="stylus" scoped>
+@import '~assets/variables'
+
+header
+	padding 5mm 0
+
+section
+	text-align center
+
+.logo
+	display block
+	margin 0 auto
+	max-width 800px
+	width 95vw
+
+.tezonator-photo
+	border 2px solid brand-purple
+	display block
+	margin 5mm auto
+	max-width 700px
+	padding 0
+	width 95%
+
+.button
+	margin 10mm
+</style>
+
